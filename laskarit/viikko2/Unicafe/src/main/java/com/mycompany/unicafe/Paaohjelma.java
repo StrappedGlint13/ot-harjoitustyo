ffd25ff01244316de3096561fd8f9a66f99c38c4
StrappedGlint13 <matiasbrax@gmail.com>
2019-11-18 22:08:26 +0200

    target

--- laskarit/viikko2/Unicafe/src/main/java/com/mycompany/unicafe/Paaohjelma.java
+++ laskarit/viikko2/Unicafe/src/main/java/com/mycompany/unicafe/Paaohjelma.java
@@ -3,6 +3,15 @@ package com.mycompany.unicafe;
 public class Paaohjelma {
 
     public static void main(String[] args) {
+         double i = 0.9;
+         double uusiJengi = 35;
+         int vuosi = 20;
+         double t= 500;
+         for (int j = 0; j < 20; j++) {
+            t = 0.9*t+35;
+             //System.out.println(t);
+        }
+         System.out.println(t);
         
     }
 }
