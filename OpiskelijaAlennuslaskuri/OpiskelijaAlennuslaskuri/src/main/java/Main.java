dbe4fd99818c0d31ca9c68a50e25a03a44d935a7
StrappedGlint13 <matiasbrax@gmail.com>
2019-11-19 11:20:58 +0200

    test

--- OpiskelijaAlennuslaskuri/OpiskelijaAlennuslaskuri/src/main/java/Main.java
+++ OpiskelijaAlennuslaskuri/OpiskelijaAlennuslaskuri/src/main/java/Main.java
@@ -1,5 +1,7 @@
 
 import java.util.Scanner;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -11,11 +13,17 @@ import java.util.Scanner;
  *
  * @author matibrax
  */
-public class Main {
+public class Main extends Application {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
         System.out.println("kokeillaanpas");
 
     }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
 }
